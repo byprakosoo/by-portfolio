628e8df733a1bc508bff045f992ac5368d502a95
Bayu Prakoso <bayuprakoso.adi@gmail.com>
2023-01-19 13:33:55 +0700

    add dribbble icon

---
+++ src/gatsby-theme-mate/icon.js
@@ -0,0 +1,22 @@
+import {
+    faMedium,
+    faTwitter,
+    faGithub,
+    faDribbble,
+  } from '@fortawesome/free-brands-svg-icons';
+  import {
+    faGlobe,
+    faEnvelope,
+    faCoffee,
+    faDribbble,
+  } from '@fortawesome/free-solid-svg-icons';
+  
+  export const ICONS = [
+    faMedium,
+    faTwitter,
+    faGithub,
+    faGlobe,
+    faEnvelope,
+    faCoffee,
+    faDribbble
+  ];
